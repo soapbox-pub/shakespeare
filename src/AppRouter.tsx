0986dd4e9d218ab74f3a3cc7bef38ea8e5388083
Alex Gleason <alex@alexgleason.me>
2025-07-20 15:45:07 -0500

    ShakespeareHome -> Index

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,7 +4,6 @@ import { ScrollToTop } from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import { NIP19Page } from "./pages/NIP19Page";
 import NotFound from "./pages/NotFound";
-import { ShakespeareHome } from "./pages/ShakespeareHome";
 import { ProjectView } from "./pages/ProjectView";
 
 export function AppRouter() {
@@ -12,9 +11,8 @@ export function AppRouter() {
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<ShakespeareHome />} />
+        <Route path="/" element={<Index />} />
         <Route path="/project/:projectId" element={<ProjectView />} />
-        <Route path="/legacy" element={<Index />} />
         {/* NIP-19 route for npub1, note1, naddr1, nevent1, nprofile1 */}
         <Route path="/:nip19" element={<NIP19Page />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}

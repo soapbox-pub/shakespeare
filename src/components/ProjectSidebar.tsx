ebc4c2d99709d01a780ee8e2179985827e151a59
Alex Gleason <alex@alexgleason.me>
2025-09-13 18:59:13 -0500

    Fix position of star icon in ProjectSidebar

--- src/components/ProjectSidebar.tsx
+++ src/components/ProjectSidebar.tsx
@@ -57,7 +57,7 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project, isSelected, onSelect
 
             {/* Star indicator - only shown when favorited */}
             {isFavorite && (
-              <div className="flex-shrink-0 relative -mr-2 pointer-events-none">
+              <div className="flex-shrink-0 relative -mr-1 pointer-events-none">
                 <div className="flex items-center justify-center">
                   <Star className="h-4 w-4 text-yellow-500 fill-current" />
                 </div>

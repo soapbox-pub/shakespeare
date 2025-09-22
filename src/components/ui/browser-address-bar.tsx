aea8c1390b565c4faaab09bbc1659ee5476b9d7b
Alex Gleason <alex@alexgleason.me>
2025-09-22 15:23:14 -0500

    Add max width to preview address bar

--- src/components/ui/browser-address-bar.tsx
+++ src/components/ui/browser-address-bar.tsx
@@ -88,7 +88,7 @@ export function BrowserAddressBar({
       </div>
 
       {/* Address bar */}
-      <form onSubmit={handleSubmit} className="flex-1 mx-2">
+      <form onSubmit={handleSubmit} className="flex-1 mx-auto px-2 max-w-2xl">
         <Input
           type="text"
           value={inputValue}

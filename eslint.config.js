920b4c4426b228b13b510ee0e82ed6e2f286255e
sam <samt@samt.st>
2025-10-04 15:54:35 +0700

    lint config

--- eslint.config.js
+++ eslint.config.js
@@ -45,6 +45,7 @@ export default tseslint.config(
         "error",
         { terms: ["fixme"] },
       ],
+      "no-control-regex": "off", // Allow ANSI escape sequences in regex
     },
   },
   {

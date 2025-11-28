68eee82c920488a569273d566b409e8c2b21737e
Alex Gleason <alex@alexgleason.me>
2025-11-28 15:10:49 -0600

    Make everything a bit rounder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,7 +81,8 @@ export default {
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+        sm: 'calc(var(--radius) - 4px)',
+        xs: '2px'
       },
       keyframes: {
         'accordion-down': {

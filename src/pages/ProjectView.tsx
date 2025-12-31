16cb47832ec883ea5b24c31d821970c92de674cd
Alex Gleason <alex@alexgleason.me>
2025-12-31 13:22:59 -0600

    Make the ProjectView buttons more clickable on mobile

--- src/pages/ProjectView.tsx
+++ src/pages/ProjectView.tsx
@@ -227,7 +227,7 @@ export function ProjectView() {
               </div>
             </div>
 
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-4 px-3">
               {project ? (
                 <>
                   <GitSyncButton

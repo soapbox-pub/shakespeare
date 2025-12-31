9941078da15e162f8213bb0866d6b6a43b4152d1
Alex Gleason <alex@alexgleason.me>
2025-12-30 21:14:00 -0600

    Don't display cents in CreditsBadge

--- src/components/CreditsBadge.tsx
+++ src/components/CreditsBadge.tsx
@@ -28,8 +28,8 @@ export function CreditsBadge({ provider, className, onOpenDialog }: CreditsBadge
   const formattedAmount = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(credits.amount);
 
   return (
